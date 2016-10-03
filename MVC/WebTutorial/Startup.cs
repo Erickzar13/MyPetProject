4ad6d9a67e35e727d97bc6d720137ae265ba5b2d
Serhii Tyshchenko <stysh@softserveinc.com>
2016-10-03 12:02:27 +0300

    - add project with more complicated controller

---
+++ MVC/WebTutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTutorial.Startup))]
+namespace WebTutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

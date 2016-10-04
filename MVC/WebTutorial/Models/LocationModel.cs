46a666a25729019b2675f6cc3ccddf06691cc419
Serhii Tyshchenko <stysh@softserveinc.com>
2016-10-04 16:04:06 +0300

    - add new field to Location

--- MVC/WebTutorial/Models/LocationModel.cs
+++ MVC/WebTutorial/Models/LocationModel.cs
@@ -10,5 +10,9 @@ namespace WebTutorial.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
     }
 }
